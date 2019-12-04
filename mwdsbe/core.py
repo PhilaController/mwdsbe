79e78152e94114d04c2633028c145885c6cec9cd
Nick Hand <nicholas.adam.hand@gmail.com>
2019-12-04 16:09:11 -0500

    add philly MSA column to registry

--- mwdsbe/core.py
+++ mwdsbe/core.py
@@ -6,6 +6,18 @@ import numpy as np
 import os
 
 
+def _load_philly_msa():
+    """
+    Load the boundary file for the Philadelphia MSA
+    """
+    URL = "https://www2.census.gov/geo/tiger/GENZ2018/shp/cb_2018_us_cbsa_5m.zip"
+    msa = gpd.read_file(URL).query(
+        "NAME == 'Philadelphia-Camden-Wilmington, PA-NJ-DE-MD'"
+    )
+
+    return msa.to_crs(epsg=4326)
+
+
 def load_registry():
     """
     Load the geo-coded version of the OEO registry of minority/women/disabled 
@@ -26,7 +38,9 @@ def load_registry():
 
     # Convert to a GeoDataFrame
     registry["geometry"] = [Point(i, j) for i, j in zip(registry.lng, registry.lat)]
-    registry = gpd.GeoDataFrame(registry, geometry="geometry")
+    registry = gpd.GeoDataFrame(
+        registry, geometry="geometry", crs={"init": "epsg:4326"}
+    )
 
     # Set missing geometries to NaN
     invalid = registry.lat.isnull() | registry.lng.isnull()
@@ -36,6 +50,18 @@ def load_registry():
     bad_names = registry["dba_name"].str.lower().isin(["same", "none"])
     registry.loc[bad_names, "dba_name"] = np.nan
 
+    # add Philly MSA
+    msa = _load_philly_msa()
+    matched = gpd.sjoin(
+        registry.dropna(subset=["lat"]), msa.to_crs(registry.crs), op="within"
+    )
+
+    # Set the index
+    registry = registry.set_index("registry_id")
+
+    registry["in_philly_msa"] = False
+    registry.loc[matched["registry_id"], "in_philly_msa"] = True
+
     # return
-    return registry.rename_axis("registry_id")
+    return registry
 
